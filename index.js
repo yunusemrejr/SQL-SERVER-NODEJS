ac9b3354c2537cd2fb3581d4c666f81cf918439f
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2023-03-16 14:27:36 +0300

    test

---
+++ index.js
@@ -0,0 +1,8 @@
+const sql = require("msnodesqlv8");
+
+const connectionString = "server=.;Database=yunusDemo;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const query = "SELECT * FROM dbo.table1";
+
+sql.query(connectionString, query, (err, rows) => {
+    console.log(rows);
+});
