fe19185317f0e12ba58d7f51f3afdb5610071fab
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2023-03-16 14:39:24 +0300

    test

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const sql = require("msnodesqlv8");
 
-const connectionString = "server=.;Database=yunusDemo;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
-const query = "SELECT * FROM dbo.table1";
+const connectionString = "server=.;Database=master;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const query = "SELECT * FROM dbo.customers WHERE name='YUNUS EMRE VURGUN'";
 
 sql.query(connectionString, query, (err, rows) => {
     console.log(rows);
-})
-
-;
+});
